fdf7e83b92000b38958bb207a3d01012b57a17f5
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-05-10 20:26:03 -0500

    Implement linking to resource.

--- client/components/articleList.jsx
+++ client/components/articleList.jsx
@@ -1,23 +1,26 @@
 // REACT/REDUX
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ArticleList = ({ articles }) => {
 
   const renderArticles = () => articles.map((article, index) => (
-    <tr>
+    <tr key={index}>
       <td>{article.createdAt}</td>
-      <td>{article.title}</td>
+      <td><Link to={`/article/${article._id}`}>{article.title}</Link></td>
     </tr>
   ));
 
   return articles.length ? (
     <table>
-      <tr>
-        <th>Date Added</th>
-        <th>Article Title</th>
-      </tr>
-      {renderArticles()}
+      <tbody>
+        <tr>
+          <th>Date Added</th>
+          <th>Article Title</th>
+        </tr>
+        {renderArticles()}
+      </tbody>
     </table>
   ) : (
     <h3>There aren't any articles yet. Click the link above to add one.</h3>
