c554f534a407b9d270065ab41797700ed56291a3
Tyler Holzer <tyler.s.holzer@gmail.com>
2017-05-11 09:47:05 -0500

    Make articleList React component, move database call from app.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,41 +2,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 // COMPONENTS
 import Nav from './nav.jsx';
 import ArticleList from './articleList.jsx';
 import Article from './article.jsx';
 import Submit from './submit.jsx';
-import { loadArticles } from '../actions/actions.jsx';
 
-class App extends React.Component {
-
-  componentWillMount() {
-    axios.get('/api/article')
-    .then(response => {
-      this.props.dispatch(loadArticles(response.data));
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  };
-
-  render () {
-    return (
+const App = () => (
+  <div>
+    <BrowserRouter>
       <div>
-        <BrowserRouter>
-          <div>
-            <Nav />
-            <Route exact path="/" component={ArticleList} />
-            <Route path="/article/:id" component={Article} />
-            <Route path="/submit" component={Submit} />
-          </div>
-        </BrowserRouter>
+        <Nav />
+        <Route exact path="/" component={ArticleList} />
+        <Route path="/article/:id" component={Article} />
+        <Route path="/submit" component={Submit} />
       </div>
-    );
-  };
-};
+    </BrowserRouter>
+  </div>
+);
 
-export default connect()(App);
+export default App;
--- client/components/articleList.jsx
+++ client/components/articleList.jsx
@@ -3,36 +3,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setCurrent } from '../actions/actions.jsx';
+import { loadArticles } from '../actions/actions.jsx';
+import axios from 'axios';
 
-const ArticleList = ({ articles, current, dispatch }) => {
+class ArticleList extends React.Component {
 
-  const renderArticles = () => articles.map((article, index) => (
-    <tr key={index}>
-      <td>{article.createdAt}</td>
-      <td>
-        <Link
-          to={`/article/${article._id}`}
-          onClick={() => dispatch(setCurrent(article))}
-        >{article.title}
-        </Link>
+  componentWillMount() {
+    axios.get('/api/article')
+    .then(response => {
+      this.props.dispatch(loadArticles(response.data));
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  };
 
-      </td>
-    </tr>
-  ));
+  renderArticles () {
+    return this.props.articles.map((article, index) => (
+      <tr key={index}>
+        <td>{article.createdAt}</td>
+        <td>
+          <Link
+            to={`/article/${article._id}`}
+            onClick={() => this.props.dispatch(setCurrent(article))}
+          >{article.title}
+          </Link>
 
-  return articles.length ? (
-    <table>
-      <tbody>
-        <tr>
-          <th>Date Added</th>
-          <th>Article Title</th>
-        </tr>
-        {renderArticles()}
-      </tbody>
-    </table>
-  ) : (
-    <h3>There aren't any articles yet. Click the link above to add one.</h3>
-  );
+        </td>
+      </tr>
+    ));
+  };
+
+  render () {
+    return this.props.articles.length ? (
+      <table>
+        <tbody>
+          <tr>
+            <th>Date Added</th>
+            <th>Article Title</th>
+          </tr>
+          {this.renderArticles()}
+        </tbody>
+      </table>
+    ) : (
+      <h3>There aren't any articles yet. Click the link above to add one.</h3>
+    );
+  };
 };
 
 const mapStateToProps = (state) => {
